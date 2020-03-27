b72a8556f23921ea61558566bbafae310909d1a7
H-Lindner <62523318+H-Lindner@users.noreply.github.com>
2020-03-27 02:13:51 +0100

    Implemented showNeighbours
    Proper file structure this time

--- src/Objects/MineField.java
+++ src/Objects/MineField.java
@@ -111,7 +111,7 @@ public class MineField {
                 //TODO run some method to end the game
                 break;
             case Empty:
-                showNeighbours();
+                showNeighbours(x , y);
                 //TODO update Visuals
                 break;
             case Number:
@@ -124,7 +124,70 @@ public class MineField {
         }
     }
 
-    public void showNeighbours() {
-        //TODO
+    // Helping Method for showNeighbours, opens all 9 neighbouring Cells.
+    private void checkNeighbours(int x, int y){
+        for(int i = x - 1;i < x + 2; i++){
+            for(int j = y - 1;i < y + 2; j++){
+                try{
+                    getFromField(i, j).setChecked(true);
+                    // Using try-catch as a control structure in Java isn't good. Might change it later to instead have 5 seperate
+                    // blocks for the edge cases (left, top, right, down or none of those). Readability would suffer.
+                } catch(IndexOutOfBoundsException e) {}
+            }
+        }
+    }
+
+    // Called upon a click on an empty Cell(whose Coords are the argument of showNeighbours.) Will open all neighbouring empty cells and any adjacent number Cells.
+// Not using a recursive function for perfomance reasons. Instead, the method will first find the leftmost nearest Cell containing a number,
+// then go along the horizontal line of the selected Cell and open the neighbours for each empty Cell on each vertical line
+// that is perpendicular to and intersecting with the horizontal line. Same procedure is then repeated for the vertical line of the selected Cell.
+    public void showNeighbours(int x, int y) {
+        // TODO: Testing once the other classes are finished.
+
+        // Working with horizontal line of the called Cell ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Finding the coordinate of the leftmost empty cell on the horizontal line
+        int horizontalLineX = 0;
+        if (x > 0) {
+            for (int i = x;i > 0 && getFromField(i - 1, y).getType() == CellType.Empty; i--){
+                horizontalLineX = i - 1;
+            }
+        }
+        // Opening the neighbours for each Cell on each vertical, perpendicular line intersecting with the horizontal line of the original Cell
+
+        for (i = horizontalLineX;i < width && getFromField(i, y).getType() == CellType.Empty;i++){
+            // One for-loop for getting all Cells above, and one for all Cells below the horizontal line
+            // All Cells above
+            for(j = y;j => 0 && getFromField(i, j).getType == CellType.Empty; j--){
+                checkNeighbours(i, j);
+            }
+            // Below
+            for(j = y;j < height && getFromField(i, j).getType == CellType.Empty; j++){
+                checkNeighbours(i, j);
+            }
+            checkNeighbours(i, y);
+        }
+
+        // Working with vertical line of the called Cell ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Finding the coordinate of the uppermost empty Cell on the vertical line
+        int verticalLineY = 0;
+        if (y > 0) {
+            for (int i = y;i > 0 && getFromField(x, i).getType() == CellType.Empty; i--){
+                verticalLineY = i - 1;
+            }
+        }
+        // Opening the neighbours for each Cell on each horizontal, perpendicular line intersecting with the vertical line of the original Cell
+
+        for (i = verticalLineY;i < height && getFromField(x, i).getType() == CellType.Empty;i++){
+            // One for-loop for getting all Cells left, and one for all Cells right of the vertical line
+            // All Cells left
+            for(j = x;j => 0 && getFromField(j, i).getType == CellType.Empty; j--){
+                checkNeighbours(j, i);
+            }
+            // Right
+            for(j = x;j < width && getFromField(j, i).getType == CellType.Empty; j++){
+                checkNeighbours(j, i);
+            }
+            checkNeighbours(x, i);
+        }
     }
 }
